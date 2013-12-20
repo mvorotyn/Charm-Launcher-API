ae153b80613307ff47f0c40b09181984f5d7e488
mrzban <mrzban@gmail.com>
2013-12-20 09:55:54 +0200

    basic "TLauncher" class definition

--- CharmLauncherAPI/CharmLauncherAPI.pas
+++ CharmLauncherAPI/CharmLauncherAPI.pas
@@ -1,17 +1,39 @@
 unit CharmLauncherAPI;
 
-type
-  TLauncher = class
-    GameDirectory: string;
-    Logging: Boolean;
-    TLaunchInfo = record
-      JavaPath: string[500];
-      Version: string[100];
-    end;
-  end;
-
 interface
 
+
+uses
+System.SysUtils;
+
+
+type
+  TLaunchInfo = record
+    JavaPath: string[255];
+    Version: string[100];
+  end;
+  TLauncher = class(TObject)
+    Logging: Boolean;
+  private
+    FDirectory: string;
+  public
+    constructor Create(const Directory: string);
+    property Directory: string read FDirectory;
+  end;
+
+
+
 implementation
+
+
+
+{ TLauncher }
+
+constructor TLauncher.Create(const Directory: string);
+begin
+  SetCurrentDir(Directory);
+end;
+
+
 
 end.
