80f39fab428d43d936aa24fbb06553a2013620c0
mrzban <mrzban@gmail.com>
2013-12-20 08:51:30 +0200

    added base class

--- CharmLauncherAPI/CharmLauncherAPI.pas
+++ CharmLauncherAPI/CharmLauncherAPI.pas
@@ -1,5 +1,15 @@
 unit CharmLauncherAPI;
 
+type
+  TLauncher = class
+    GameDirectory: string;
+    Logging: Boolean;
+    TLaunchInfo = record
+      JavaPath: string[500];
+      Version: string[100];
+    end;
+  end;
+
 interface
 
 implementation
