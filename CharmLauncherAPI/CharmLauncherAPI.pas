c8eb7cb2451b8d772b3c8df90f948fa9229eaf60
mrzban <mrzban@gmail.com>
2013-12-20 11:02:28 +0200

    Added detection of installed versions

--- CharmLauncherAPI/CharmLauncherAPI.pas
+++ CharmLauncherAPI/CharmLauncherAPI.pas
@@ -4,7 +4,7 @@ interface
 
 
 uses
-System.SysUtils;
+System.SysUtils, System.Classes;
 
 
 type
@@ -12,13 +12,17 @@ type
     JavaPath: string[255];
     Version: string[100];
   end;
+
   TLauncher = class(TObject)
     Logging: Boolean;
   private
-    FDirectory: string;
+    FGameDirectory: string;
+    function GetVersions: TStringList;
   public
-    constructor Create(const Directory: string);
-    property Directory: string read FDirectory;
+    constructor Create(const GameDirectory: string);
+    property GameDirectory: string read FGameDirectory;
+  published
+    property Versions: TStringList read GetVersions;
   end;
 
 
@@ -29,11 +33,45 @@ implementation
 
 { TLauncher }
 
-constructor TLauncher.Create(const Directory: string);
+constructor TLauncher.Create(const GameDirectory: string);
 begin
-  SetCurrentDir(Directory);
+
+  FGameDirectory:=GameDirectory;
+  SetCurrentDir(GameDirectory);
 end;
 
+
+function TLauncher.GetVersions: TStringList;
+  var
+  searchResult : TSearchRec;
+  i : integer;
+begin
+  Result:=TStringList.Create;
+
+  if FindFirst(FGameDirectory + '\versions\' + '*', faDirectory, searchResult) = 0 then
+  begin
+    repeat
+      if (searchResult.attr and faDirectory) = faDirectory
+      then Result.add(searchResult.Name);
+    until FindNext(searchResult) <> 0;
+    FindClose(searchResult);
+
+    //Clearing Results
+    Result.Delete(0);
+    Result.Delete(0);
+
+    if Result.Count > 0 then
+    begin
+      for i := 0 to Result.Count-1 do
+      begin
+        if not FileExists(FGameDirectory + '\versions\' + Result[i] + '\' + Result[i] + '.json') then
+          Result.Delete(i);
+       end;
+    end else
+      raise Exception.Create('No versions installed!');
+  end;
+
+end;
 
 
 end.
