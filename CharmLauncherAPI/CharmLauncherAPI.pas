eb8fb0b609c5a45b728791f1ab6445c91cb83491
mrzban <mrzban@gmail.com>
2013-12-21 08:48:30 +0200

    many changes

--- CharmLauncherAPI/CharmLauncherAPI.pas
+++ CharmLauncherAPI/CharmLauncherAPI.pas
@@ -5,7 +5,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
-  Vcl.Forms, Vcl.Dialogs, DBXJSON;
+  Vcl.Forms, Vcl.Dialogs, DBXJSON, ShellApi;
 
 
 type
@@ -25,16 +25,18 @@ type
 
   TLaunchInfo = record
     JavaPath: string[255];
-    Version: string[100];
-    GameLibs: string[255];
     GarbageCollectorEnabled: Boolean;
     MaxHeapSizeGb: string[2];
+    Version: string[100];
+    GameLibs: string;
+    GameMainClass: string[100];
     auth_player_name: string[255];
     auth_uuid: string[255];
     auth_access_token: string[255];
     twitch_access_token: string[255];
     assets_index_name:  string[20];
     assets_root: string[100];
+    minecraft_arguments: string[255];
 
   end;
 
@@ -42,18 +44,24 @@ type
     Logging: Boolean;
   private
     FGameDirectory: string;
+    FLog: TStringList;
     function GetVersions: TStringList;
     Function DetectRam : integer;
+    function ReplaceStr(Str, X, Y: string): string;
+    procedure AddLog(text: string);
+    function PrepareArgs(minecraft_arguments: string; LaunchInfo: TLaunchInfo): string;
   public
     constructor Create(const GameDirectory: string);
     function JsonParse(RawJsonFile: string): FJSONObject;
     function JsonExtractLibs(SourceObject: FJSONObject): string;
     function JsonExtractMainClass(SourceObject: FJSONObject): string;
     function JsonExtractAssetIndex(SourceObject: FJSONObject): string;
+    function JsonExtractMinecraftArguments(SourceObject: FJSONObject): string;
     function LaunchGame(LaunchInfo: TLaunchInfo): integer;
     property GameDirectory: string read FGameDirectory;
     property AvailableRAM: integer read DetectRAM;
     function FindJavaPath: String;
+    property Log: TStringList read FLog;
   published
     property Versions: TStringList read GetVersions;
   end;
@@ -70,11 +78,55 @@ begin
 
   FGameDirectory:=GameDirectory;
   SetCurrentDir(GameDirectory);
+  FLog:=TstringList.Create;
+  AddLog('Launcher started');
+end;
+
+
+procedure TLauncher.AddLog(text: string);
+begin
+  Flog.Add(timetostr(now) + ' ' + text);
 end;
 
 
 function TLauncher.LaunchGame(LaunchInfo: TLaunchInfo): integer;
+var
+  LaunchStr: String;
+  GameJar: String;
+  GameArgs: String;
 begin
+  ShowMessage('1');
+
+  LaunchStr:='';
+  if LaunchInfo.Version = ''then
+    exit;
+  if LaunchInfo.MaxHeapSizeGb = '' then
+    exit;
+  if LaunchInfo.GameMainClass = '' then
+    exit;
+  if LaunchInfo.minecraft_arguments = '' then
+    exit;
+
+   GameJar:='versions\' + LaunchInfo.Version + '\' + LaunchInfo.Version + '.jar';
+   GameArgs:=PrepareArgs(LaunchInfo.minecraft_arguments, LaunchInfo);
+
+  if (LaunchInfo.GarbageCollectorEnabled = True) then
+    LaunchStr:=LaunchStr + '-Xincgc ';
+  LaunchStr:=LaunchStr + '-Xmx' + LaunchInfo.MaxHeapSizeGb + 'g ';
+  LaunchStr:=LaunchStr + '-Djava.library.path=versions\' + LaunchInfo.Version +'\natives ';
+  LaunchStr:=LaunchStr + '-cp ' + LaunchInfo.GameLibs + GameJar + ' ';
+  LaunchStr:=LaunchStr + LaunchInfo.GameMainClass + ' ';
+  LaunchStr:=LaunchStr + GameArgs;
+
+  AddLog('Launch command: ' + LaunchStr);
+
+    ShellExecute(Application.Handle,
+               'Open',
+               Pchar('C:\Users\home\Desktop\Java64\bin\java.exe'),
+               PChar(LaunchStr),
+               nil,
+               SW_SHOW );
+
 
 end;
 
@@ -241,6 +293,7 @@ begin
       Result:=SourceObject.Get('mainClass').JsonValue.Value
   end;
 
+  Enum.Free;
 end;
 
 
@@ -263,6 +316,29 @@ begin
       Result:=SourceObject.Get('assets').JsonValue.Value
   end;
 
+  Enum.Free;
+end;
+
+function TLauncher.JsonExtractMinecraftArguments(SourceObject: FJSONObject): string;
+var
+  Enum: TStringList;
+  i: integer;
+begin
+  Result:='';
+  if SourceObject.Size < 1 then
+    exit;
+
+  Enum:=TstringList.Create;
+  for i := 0 to SourceObject.Size-1 do
+    Enum.Add((SourceObject.Get(i)).JsonString.Value);
+
+  for i := 0 to Enum.Count-1 do
+  begin
+    if Enum.Strings[i]='minecraftArguments' then
+      Result:=SourceObject.Get('minecraftArguments').JsonValue.Value
+  end;
+
+  Enum.Free;
 end;
 
 function TLauncher.FindJavaPath: String;
@@ -287,6 +363,53 @@ begin
 
   if FileExists(buff + '\Program Files(x86)\Java\jre6\bin\javaw.exe') then
     Result:=buff + '\Program Files(x86)\Java\jre6\';
+end;
+
+
+function TLauncher.ReplaceStr(Str, X, Y: string): string;
+var
+  buf1, buf2, buffer: string;
+  i: Integer;
+
+begin
+  buf1 := '';
+  buf2 := Str;
+  Buffer := Str;
+
+  while Pos(X, buf2) > 0 do
+  begin
+    buf2 := Copy(buf2, Pos(X, buf2), (Length(buf2) - Pos(X, buf2)) + 1);
+    buf1 := Copy(Buffer, 1, Length(Buffer) - Length(buf2)) + Y;
+    Delete(buf2, Pos(X, buf2), Length(X));
+    Buffer := buf1 + buf2;
+  end;
+
+  ReplaceStr := Buffer;
+end;
+
+
+function TLauncher.PrepareArgs(minecraft_arguments: string; LaunchInfo: TLaunchInfo): string;
+begin
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${auth_session}', LaunchInfo.auth_uuid) ;
+
+  //modern 1.6.1+
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${auth_player_name}', LaunchInfo.auth_player_name) ;
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${version_name}', LaunchInfo.Version) ;
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${game_directory}', '.\') ;
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${game_assets}', LaunchInfo.assets_root) ;  // assets\virtual\legacy
+
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${auth_uuid}', LaunchInfo.auth_uuid) ;
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${auth_access_token}', LaunchInfo.auth_access_token) ;
+
+  //1.7.2+
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${user_properties}', '{"twitch_access_token":["' + LaunchInfo.twitch_access_token + '"]}') ;
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${user_type}', 'legacy') ;
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${assets_root}', LaunchInfo.assets_root);
+  minecraft_arguments:=ReplaceStr(minecraft_arguments, '${assets_index_name}', LaunchInfo.assets_index_name) ;
+
+  Result:=minecraft_arguments;
+
+
 end;
 
 end.
